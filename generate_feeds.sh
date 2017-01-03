f0fdfd72319f34cfc6535e2c70e5bb29b6a99c7a
Matt Steen <mattsteen@gmail.com>
2017-01-02 21:00:58 -0500

    Correcting outdated path

--- generate_feeds.sh
+++ generate_feeds.sh
@@ -1,3 +1,3 @@
 ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/atlantic_config.xml > /tmp/feed.txt
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-cp /var/www/html/feeds/the_atlantic/trump.xml /home/ubuntu/feeds_control/trump/trump_$timestamp
+cp /var/www/html/feeds/the_atlantic_monthly/trump.xml /home/ubuntu/feeds_control/trump/trump_$timestamp
