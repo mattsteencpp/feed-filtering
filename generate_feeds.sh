6ddd92e03fe3076b3411d6bb9d383f8c26ad6515
Matt Steen <mattsteen@gmail.com>
2017-01-07 18:20:47 -0500

    Adding a feed for the On Being podcast

--- generate_feeds.sh
+++ generate_feeds.sh
@@ -1,4 +1,5 @@
-ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/atlantic_config.xml > /tmp/feed.txt
-ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/arseblog_config.xml > /tmp/feed.txt
+ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/atlantic_config.xml > /tmp/atlantic_feed.txt
+ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/arseblog_config.xml > /tmp/arseblog_feed.txt
+ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/on_being_config.xml > /tmp/on_being_feed.txt
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 cp /var/www/html/feeds/the_atlantic_monthly/trump.xml /home/ubuntu/feeds_control/trump/trump_$timestamp
--- test_generate.sh
+++ test_generate.sh
@@ -1,2 +1,3 @@
-ruby generate_feeds.rb atlantic_config.xml /home/msteen/projects/ruby/the_atlantic 
-ruby generate_feeds.rb arseblog_config.xml /home/msteen/projects/ruby/arseblog
+#ruby generate_feeds.rb atlantic_config.xml /home/msteen/projects/ruby/the_atlantic 
+#ruby generate_feeds.rb arseblog_config.xml /home/msteen/projects/ruby/arseblog
+ruby generate_feeds.rb on_being_config.xml /home/msteen/projects/ruby/on_being
