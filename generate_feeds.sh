b2b279b0e3fe84ee16d57bd7b7b24376caeb5e95
Matt Steen <mattsteen@gmail.com>
2017-01-09 20:06:46 -0500

    Adding support for the Fresh Air podcast feed

--- generate_feeds.sh
+++ generate_feeds.sh
@@ -1,5 +1,6 @@
 ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/atlantic_config.xml > /tmp/atlantic_feed.txt
 ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/arseblog_config.xml > /tmp/arseblog_feed.txt
 ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/on_being_config.xml > /tmp/on_being_feed.txt
+ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/fresh_air_config.xml > /tmp/fresh_air_feed.txt
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 cp /var/www/html/feeds/the_atlantic_monthly/trump.xml /home/ubuntu/feeds_control/trump/trump_$timestamp
--- test_generate.sh
+++ test_generate.sh
@@ -1,3 +1,4 @@
 #ruby generate_feeds.rb atlantic_config.xml /home/msteen/projects/ruby/the_atlantic 
 #ruby generate_feeds.rb arseblog_config.xml /home/msteen/projects/ruby/arseblog
-ruby generate_feeds.rb on_being_config.xml /home/msteen/projects/ruby/on_being
+#ruby generate_feeds.rb on_being_config.xml /home/msteen/projects/ruby/on_being
+ruby generate_feeds.rb fresh_air_config.xml /home/msteen/projects/ruby/fresh_air
