bd52426f06d7e9ddb3ac194015ed8bc9e22130e7
Matt Steen <mattsteen@gmail.com>
2017-01-20 18:12:07 -0500

    Adding a feed for planet money, with episode number filtering

--- generate_feeds.sh
+++ generate_feeds.sh
@@ -3,3 +3,4 @@ ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/ars
 ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/on_being_config.xml > /tmp/on_being_feed.txt
 ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/fresh_air_config.xml > /tmp/fresh_air_feed.txt
 ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/freakonomics_config.xml > /tmp/freakonomics_feed.txt
+ruby /home/ubuntu/feeds_control/generate_feeds.rb /home/ubuntu/feeds_control/planet_money_config.xml > /tmp/planet_money_feed.txt
--- test_generate.sh
+++ test_generate.sh
@@ -2,4 +2,5 @@
 #ruby generate_feeds.rb arseblog_config.xml /home/msteen/projects/ruby/arseblog
 #ruby generate_feeds.rb on_being_config.xml /home/msteen/projects/ruby/on_being
 #ruby generate_feeds.rb fresh_air_config.xml /home/msteen/projects/ruby/fresh_air
-ruby generate_feeds.rb freakonomics_config.xml /home/msteen/projects/ruby/freakonomics
+#ruby generate_feeds.rb freakonomics_config.xml /home/msteen/projects/ruby/freakonomics
+ruby generate_feeds.rb planet_money_config.xml /home/msteen/projects/ruby/planet_money
